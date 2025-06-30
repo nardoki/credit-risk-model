9a8bc4d0ac6b56f1af791ee9a20c527797ae612b
mihret habtamu <116177887+nardoki@users.noreply.github.com>
2025-06-30 22:45:25 +0300

    feat: deploy ML model with FastAPI

--- src/api/pydantic_models.py
+++ src/api/pydantic_models.py
@@ -1,10 +1,13 @@
 from pydantic import BaseModel
 
+
+
 class CreditRiskRequest(BaseModel):
     age: float
     income: float
     loan_amount: float
     # add other numeric features here
+
 
 class CreditRiskResponse(BaseModel):
     risk_probability: float
