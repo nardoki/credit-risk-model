fe5726984d2cdd495672bb56bba25bb4d65dd7af
mihret habtamu <116177887+nardoki@users.noreply.github.com>
2025-06-30 22:42:56 +0300

    Update main.py

--- src/api/main.py
+++ src/api/main.py
@@ -8,9 +8,11 @@ app = FastAPI()
 MODEL_NAME = "random_forest"   # Replace with your registered model name
 MODEL_VERSION = "1"            # Use the correct version
 
-model = mlflow.pyfunc.load_model("mlruns\975347351455683709\models\m-add428fdfa0149d48a24387cb8a1f5e6\artifacts")
+model = mlflow.pyfunc.load_model(r"mlruns\975347351455683709\models\m-add428fdfa0149d48a24387cb8a1f5e6\artifacts")
+
 
 @app.post("/predict", response_model=CreditRiskResponse)
+
 def predict_risk(request: CreditRiskRequest):
     input_data = pd.DataFrame([request.dict()])
     try:
